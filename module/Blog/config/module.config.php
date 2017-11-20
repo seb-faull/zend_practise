c057f97584e94c0b83b65268c1eb1cab50499cee
seb-faull <seb_faull@hotmail.co.uk>
2017-11-20 14:07:58 +0000

    Added Index Controller class to Blog\Controller

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -3,9 +3,9 @@
 namespace Blog;
 
 return array(
-  'controllers' => [
-    'factories' => [
-        Blog\Controller\IndexController::class => InvokableFactory::class,
+    'controllers' => [
+        'factories' => [
+            Blog\Controller\IndexController::class => InvokableFactory::class,
+        ],
     ],
-  ],
 );
