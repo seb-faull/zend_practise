ae5f518c372701efc5967f26f822ba3664a79b4a
seb-faull <seb_faull@hotmail.co.uk>
2017-11-20 12:22:24 +0000

    Added config functions to Blog/Module

--- module/Blog/Module.php
+++ module/Blog/Module.php
@@ -9,4 +9,35 @@ use Zend\ModuleManager\Feature\ServiceProviderInterface;
 class Module implements ConfigProviderInterface, ServiceProviderInterface, AutoloaderProviderInterface
 {
 
+  //* Return an array for passing to Zend\Loader\AutoloaderFactory.
+  //* @return array
+  public function getAutoLoaderConfig()
+  {
+    return array(
+      'Zend\Loader\StandardAutoLoader' => array(
+        'namespaces' => array(
+          __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+        ),
+      ),
+    );
+  }
+
+  //* Returns configuration to merge with application configuration
+  //* @return array|\Traversable
+  public function getConfig()
+  {
+    return include __DIR__ . '/config/module.config.php';
+  }
+
+  //* Expected to return \Zend\ServiceManager\Config object or array to
+  //* seed such an object.
+  //*s
+  //* @return array|\Traversable
+  public function getServiceConfig()
+  {
+    return include __DIR__ . '/config/service.config.php';
+  }
+
+
+
 }
