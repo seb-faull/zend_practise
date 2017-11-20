20da6b5018f1f37133cd53a47d44ceceeeac6839
seb-faull <seb_faull@hotmail.co.uk>
2017-11-20 15:22:10 +0000

    Almost getting route /blog to work

--- module/Blog/src/Blog/Controller/IndexController.php
+++ module/Blog/src/Blog/Controller/IndexController.php
@@ -7,7 +7,7 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction
+    public function indexAction()
     {
         return new ViewModel();
     }
