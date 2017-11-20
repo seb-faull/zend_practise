b4ae06f273df5f56cffcdea3ce978577359e273e
seb-faull <seb_faull@hotmail.co.uk>
2017-11-20 14:26:46 +0000

    Added routes to Blog\module.config

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -3,9 +3,31 @@
 namespace Blog;
 
 return array(
+
+    'router' => [
+        'routes' => [
+            'blog' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/blog[/:action]',
+                    'defaults' => [
+                        'controller' => Blog\Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'controllers' => [
         'factories' => [
             Blog\Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
         ],
     ],
 );
