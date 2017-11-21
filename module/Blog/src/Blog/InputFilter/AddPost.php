1eedd40f75b5f058f522e924ccd9b478429a66e1
seb-faull <seb_faull@hotmail.co.uk>
2017-11-21 14:30:33 +0000

    Implementing slug validator chain

--- module/Blog/src/Blog/InputFilter/AddPost.php
+++ module/Blog/src/Blog/InputFilter/AddPost.php
@@ -13,8 +13,24 @@ class AddPost extends InputFilter
     {
         $title = new Input('title');
         $title->setRequired(true);
+        $title->setValidatorChain($this->getTitleValidatorChain());
 
+        $slug = new Input('slug');
+        $slug->setRequired(true);
+        $slug->setValidatorChain($this->getSlugValidatorChain());
+    }
 
+    protected function getSlugValidatorChain()
+    {
+      // Title's Minimum & Maximum character length
+      $stringLength = new StringLength();
+      $stringLength->setMin(5);
+      $stringLength->setMax(50);
+
+      $validatorChain = new ValidatorChain();
+      $validatorChain->attach($stringLength);
+
+      return $validatorChain;
     }
 
     protected function getTitleValidatorChain()
@@ -26,5 +42,8 @@ class AddPost extends InputFilter
 
         $validatorChain = new ValidatorChain();
         $validatorChain->attach(new Alnum(true));
+        $validatorChain->attach($stringLength);
+
+        return $validatorChain;
     }
 }
