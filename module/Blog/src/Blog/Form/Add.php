bc09108d2e05b8929d107fa5bbeb0e5d4dfdb943
seb-faull <seb_faull@hotmail.co.uk>
2017-11-21 11:05:15 +0000

    Initialised the Title element for Form

--- module/Blog/src/Blog/Form/Add.php
+++ module/Blog/src/Blog/Form/Add.php
@@ -1,9 +1,20 @@
 <?php
 
 namespace Blog\Form;
+
 use Zend\Form\Form;
+use Zend\Form\Element;
 
 class Add extends Form
 {
+    // Perform the initialization of the class
+    // which involves adding elements to the form.
+    public function __construct()
+    {
+        parent:: __construct('add');
 
+        $title = new Element\Text('title');
+        $title->setLabel('Title');
+        $title->setAttribute('class', 'form-control');
+    }
 }
