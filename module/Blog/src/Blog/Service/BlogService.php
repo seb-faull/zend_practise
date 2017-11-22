2f11e941cf9b071d47db622b3ee8faae1fe35560
seb-faull <seb_faull@hotmail.co.uk>
2017-11-22 11:39:26 +0000

    Added function Save post

--- module/Blog/src/Blog/Service/BlogService.php
+++ module/Blog/src/Blog/Service/BlogService.php
@@ -2,7 +2,13 @@
 
 namespace Blog\Service;
 
+use Blog\Entity\Post;
+
 interface BlogService
 {
-  
+    /*
+     * Saves a blog post
+     * Returns @Post 
+     */
+    public function save(Post $post);
 }
