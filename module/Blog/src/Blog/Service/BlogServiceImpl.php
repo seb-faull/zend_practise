c6f95a99e6fcc1e0666983bb4a4a2091af60305d
seb-faull <seb_faull@hotmail.co.uk>
2017-11-22 11:42:51 +0000

    Created a class for Blog Service Implementation

---
+++ module/Blog/src/Blog/Service/BlogServiceImpl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blog\Service;
+
+use Blog\Entity\Post;
+
+class BlogServiceImpl implements BlogService
+{
+    public function save(Post $post)
+    {
+        //ToDo: Save Post
+    }
+}
